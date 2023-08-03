3521bcc419cbcb3babd60a0660b0c636d76dd8fe
WI Jeong <cpdls23@naver.com>
2023-08-03 16:05:18 +0900

    Text Rpg Practice

--- TextRPG/TextRPG.cpp
+++ TextRPG/TextRPG.cpp
@@ -2,13 +2,13 @@
 #include <string>
 
 
-enum MainMenu 
+enum MainMenu
 {
 	MM_None,
 	MM_Battle,
 	MM_Store,
 	MM_Inventory,
-	MM_Exit 
+	MM_Exit
 };
 
 enum BattlePage
@@ -28,9 +28,22 @@ enum Job
 	J_Wizard
 };
 
+enum Battle
+{
+	B_None,
+	B_Attack,
+	B_Back
+};
+
+struct Inventory
+{
+	int IGold;
+};
+
+
 struct Player
 {
-	char P_Name[32]; 
+	char P_Name[32];
 	char P_JobName[32];
 	Job eJob;
 	int P_AttackMax;
@@ -43,8 +56,10 @@ struct Player
 	int P_MPMax;
 	int P_Exp;
 	int P_Level;
-
+	Inventory eInventory;
 };
+
+
 
 struct Monster
 {
@@ -67,6 +82,9 @@ struct Monster
 
 int main()
 {
+	srand((unsigned int)time(0));
+	rand();
+
 	char str[32];
 
 	Player player = {};
@@ -75,7 +93,7 @@ int main()
 
 
 	int iJob = J_None;
-	
+
 	while (iJob == J_None)
 	{
 		system("cls");
@@ -86,13 +104,18 @@ int main()
 		std::cin >> iJob;
 
 		if (iJob <= J_None || iJob > J_Wizard)
-			iJob = J_None; 
+			iJob = J_None;
 
 	}
+
 	player.P_Level = 1;
 	player.P_Exp = 0;
-
+	player.eInventory.IGold = 10000;
 	player.eJob = (Job)iJob;
+
+
+
+
 	switch (player.eJob)
 	{
 	case J_Knight:
@@ -108,7 +131,7 @@ int main()
 		break;
 	case J_Archer:
 		strcpy_s(player.P_JobName, "궁수");
-		player.P_AttackMin =10;
+		player.P_AttackMin = 10;
 		player.P_AttackMax = 15;
 		player.P_ArmorMin = 10;
 		player.P_ArmorMax = 15;
@@ -116,8 +139,8 @@ int main()
 		player.P_HP = 400;
 		player.P_MP = 200;
 		player.P_MPMax = 200;
-			break;
-	case J_Wizard: 
+		break;
+	case J_Wizard:
 		strcpy_s(player.P_JobName, "마법사");
 		player.P_AttackMin = 15;
 		player.P_AttackMax = 20;
@@ -127,7 +150,7 @@ int main()
 		player.P_HP = 300;
 		player.P_MP = 300;
 		player.P_MPMax = 300;
-			break;
+		break;
 	}
 
 	Monster MonsterArr[BP_Back - 1] = {};
@@ -177,7 +200,6 @@ int main()
 	MonsterArr[2].M_GoldMin = 20000;
 	MonsterArr[2].M_GoldMax = 50000;
 
-	
 
 	while (true)
 	{
@@ -212,21 +234,71 @@ int main()
 				std::cout << "3. 어려움" << std::endl;
 				std::cout << "4. 뒤로가기" << std::endl;
 				std::cout << "메뉴를 선택하세요 : ";
-				std::cin >> input;  
+				std::cin >> input;
 
-				if (input <= BP_None || input > BP_Back) 
+				if (input <= BP_None || input > BP_Back)
 					continue;
-				else if (input == BP_Back)  
+				else if (input == BP_Back)
 					break;
 			}
-			break;
-		case MM_Store: 
+
+			Monster monster = MonsterArr[input - 1];
+
+			while (true)
+			{
+				switch (input)
+				{
+				case BP_Easy:
+					std::cout << "+++++++++++++++++++++++++쉬움++++++++++++++++++++++++" << std::endl;
+					break;
+				case BP_Normal:
+					std::cout << "++++++++++++++++++++++++보통++++++++++++++++++++++++" << std::endl;
+					break;
+				case BP_Hard:
+					std::cout << "++++++++++++++++++++++++어려움++++++++++++++++++++++++" << std::endl;
+					break;
+				}
+					std::cout << "++++++++++++++++++++++++플레이어++++++++++++++++++++++++" << std::endl;
+					std::cout << "이름 : " << player.P_Name << "\t직업 : " << player.P_JobName << std::endl;
+					std::cout << "레벨 : " << player.P_Level << "\t경험치 : " << player.P_Exp << std::endl;
+					std::cout << "공격력 : " << player.P_AttackMin << "~" << player.P_AttackMax << "방어력 : " << player.P_ArmorMin << "~" << player.P_ArmorMax << std::endl;
+					std::cout << "체력 : " << player.P_HP << "/" << player.P_HPMax << "마나 : " << player.P_MP << "/" << player.P_MPMax << std::endl;
+					std::cout << "보유골드 : " << player.eInventory.IGold << "Gold : " << std::endl << "\n";
+
+					std::cout << "++++++++++++++++++++++++몬스터++++++++++++++++++++++++" << std::endl;
+					std::cout << "이름 : " << monster.M_Name << "\t레벨 : " << std::endl;
+					std::cout << "공격력 : " << monster.M_AttackMin << "~" << monster.M_AttackMax << "방어력 : " << monster.M_ArmorMin << "~" << monster.M_ArmorMax << std::endl;
+					std::cout << "체력 : " << monster.M_HP << "/" << monster.M_HPMax << "마나 : " << monster.M_MP << "/" << monster.M_MPMax << std::endl;
+					std::cout << "획득경험치 : " << monster.M_Exp << "\t획득골드" << monster.M_GoldMin << "~" << monster.M_GoldMax << std::endl << "\n";
+
+					std::cout << "1.공격" << std::endl; 
+					std::cout << "2. 도망가기" << std::endl;
+					std::cout << "메뉴를 선택하세요" << std::endl;
+					std::cin >> input;
+
+					if (input == B_Back)
+						break;
+					else if (input <= B_None || input > B_Back)
+						continue;
+
+					switch (input)
+					{
+					case B_Attack:
+						int P_AttackMin = rand() % (player.P_AttackMax - player.P_AttackMin+1) + player.P_AttackMin;
+						break;
+						
+					}
+
+
+		case MM_Store:
 			break;
 		case  MM_Inventory:
 			break;
 		default:
-		std::cout << "잘못 입력했습니다." << std::endl;
+			std::cout << "잘못 입력했습니다." << std::endl;
 			break;
+				
+			}
 		}
 	}
 
